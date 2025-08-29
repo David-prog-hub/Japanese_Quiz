203dbe15decc569cf9ce934b3cb2778c89884cde
David-prog-hub <drichter30@yahoo.com>
2025-08-29 11:54:15 +0800

    Add files via upload

---
+++ Jap_Quiz6.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import streamlit as st
+
+st.title("Japanese Quiz")
+
+# Question 1
+st.subheader("1. What does '猫' mean?")
+q1 = st.radio("Choose one:", ["Dog", "Cat", "Fish"])
+
+# Question 2
+st.subheader("2. How do you say 'ありがとう' in Japanese?")
+q2 = st.radio("Choose one:", ["Konnichiwa", "Arigatou", "Sayonara"])
+
+# Submit button
+if st.button("Submit"):
+    score = 0
+    if q1 == "Cat":
+        score += 1
+    if q1 !="Cat":
+        print("Wrong answer")
+    if q2 == "Arigatou":
+        score += 1
+    if q2 != "Arigatou":
+        print("Wrong anser")
+    st.success(f"You scored {score}/2")
+
+
+# In[1]:
+
+
+get_ipython().system('python -m nbconvert --to script Jap_Quiz5.ipynb')
+
